cca73e92d436fafc4ed6fbee5f20c16b371e333b
qiubin <31343400@qq.com>
2024-04-18 21:28:52 +0800

    add mysql execute

--- out/artifacts/first_web_war_exploded/index.jsp
+++ out/artifacts/first_web_war_exploded/index.jsp
@@ -1,14 +1,14 @@
 <%--
   Created by IntelliJ IDEA.
   User: 31343
-  Date: 2024/4/7
+  Date: 2024/4/18
   Time: 9:56
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-    <title>$Title$</title>
+    <title>My first JSP Web</title>
   </head>
   <body>
   <%
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,63 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args){
+
+    private static String username = "itat";
+    private static String password = "123";
+    private static String database = "itat_emp";
+    public static Connection getConnection() {
+        Connection con = null;
+        String url = "jdbc:mysql://localhost:3306/"+database;
+        try {
+            con = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+    public static void close(Connection con) {
+        try {
+            if(con!=null) con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void close(PreparedStatement ps) {
+        try {
+            if(ps!=null) ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void close(ResultSet rs) {
+        try {
+            if(rs!=null) rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws SQLException {
         System.out.println("hello world");
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<User> users = new ArrayList<User>();
+        con = getConnection();
+        String sql = "select * from t_user";
+        ps = con.prepareStatement(sql);
+        rs = ps.executeQuery();
+        User u = null;
+        while(rs.next()) {
+            u = new User();
+            u.setUsername(rs.getString("username"));
+            u.setPassword(rs.getString("password"));
+            u.setNickname(rs.getString("nickname"));
+            users.add(u);
+        }
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("数据驱动加载成功");
