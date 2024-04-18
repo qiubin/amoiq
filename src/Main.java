729eea8b9f01c8e7e23894cdcd9e84ede67321c2
qiubin <31343400@qq.com>
2024-04-18 21:49:37 +0800

    modify123

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws SQLException {
         System.out.println("hello world");
+
         Connection con = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
