96d4d875da7166121f7591879b625656d4159067
qiubin <31343400@qq.com>
2024-04-18 21:42:05 +0800

    add jdbcUtil class

---
+++ src/JDBCUtil.java
@@ -0,0 +1,39 @@
+import java.sql.*;
+
+public class JDBCUtil {
+    private static String username = "itat";
+    private static String password = "123";
+    private static String database = "itat_emp";
+    public static Connection getConnection() {
+        Connection con = null;
+        String url = "jdbc:mysql://localhost:3306/"+database;
+        try {
+            con = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+    public static void close(Connection con) {
+        try {
+            if(con!=null) con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void close(PreparedStatement ps) {
+        try {
+            if(ps!=null) ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void close(ResultSet rs) {
+        try {
+            if(rs!=null) rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,49 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
-    private static String username = "itat";
-    private static String password = "123";
-    private static String database = "itat_emp";
-    public static Connection getConnection() {
-        Connection con = null;
-        String url = "jdbc:mysql://localhost:3306/"+database;
-        try {
-            con = DriverManager.getConnection(url, username, password);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return con;
-    }
-    public static void close(Connection con) {
-        try {
-            if(con!=null) con.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    public static void close(PreparedStatement ps) {
-        try {
-            if(ps!=null) ps.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    public static void close(ResultSet rs) {
-        try {
-            if(rs!=null) rs.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) throws SQLException {
         System.out.println("hello world");
         Connection con = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
         List<User> users = new ArrayList<User>();
-        con = getConnection();
+        con = JDBCUtil.getConnection();
         String sql = "select * from t_user";
         ps = con.prepareStatement(sql);
         rs = ps.executeQuery();
@@ -57,12 +21,19 @@ public class Main {
             u.setNickname(rs.getString("nickname"));
             users.add(u);
         }
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("数据驱动加载成功");
-        }catch (ClassNotFoundException e){
-            e.printStackTrace();
+        for(User ue:users){
+            System.out.println("NickName:"+ue.getNickname());
+            System.out.println("password:"+ue.getPassword());
+            System.out.println("username:"+ue.getUsername());
         }
+
+            //Class.forName("com.mysql.cj.jdbc.Driver");
+            //System.out.println("数据驱动加载成功");
+            System.out.println("success!");
+
+            JDBCUtil.close(rs);
+            JDBCUtil.close(ps);
+            JDBCUtil.close(con);
+
     }
 }
