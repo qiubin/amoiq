f1229b234396fa3ff2e8c78617a98e41b52f593d
qiubin <qiubin@zoesoft.com.cn>
2024-04-19 10:09:54 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -5,36 +5,35 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws SQLException {
         System.out.println("hello world");
-
         Connection con = null;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        List<User> users = new ArrayList<User>();
-        con = JDBCUtil.getConnection();
-        String sql = "select * from t_user";
-        ps = con.prepareStatement(sql);
-        rs = ps.executeQuery();
-        User u = null;
-        while(rs.next()) {
-            u = new User();
-            u.setUsername(rs.getString("username"));
-            u.setPassword(rs.getString("password"));
-            u.setNickname(rs.getString("nickname"));
-            users.add(u);
-        }
-        for(User ue:users){
-            System.out.println("NickName:"+ue.getNickname());
-            System.out.println("password:"+ue.getPassword());
-            System.out.println("username:"+ue.getUsername());
-        }
-
-            //Class.forName("com.mysql.cj.jdbc.Driver");
-            //System.out.println("数据驱动加载成功");
+        try {
+            List<User> users = new ArrayList<User>();
+            con = JDBCUtil.getConnection();
+            String sql = "select * from t_user";
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            User u = null;
+            while (rs.next()) {
+                u = new User();
+                u.setUsername(rs.getString("username"));
+                u.setPassword(rs.getString("password"));
+                u.setNickname(rs.getString("nickname"));
+                users.add(u);
+            }
+            for (User ue : users) {
+                System.out.println("NickName:" + ue.getNickname());
+                System.out.println("password:" + ue.getPassword());
+                System.out.println("username:" + ue.getUsername());
+            }
             System.out.println("success!");
-
+        }catch (SQLException exception){
+            throw exception;
+        }finally {
             JDBCUtil.close(rs);
             JDBCUtil.close(ps);
             JDBCUtil.close(con);
-
+        }
     }
 }
